1b922945df688b17ca41c4c426259539085248df
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-13 23:36:24 +0600

    kon category te koyti news ase ta show korci

--- src/Pages/Category/Category/Category.jsx
+++ src/Pages/Category/Category/Category.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Category = () => {
+    // route a set krr por, component a loader ta call korte hobe
+    const news = useLoaderData();
+
     return (
         <div>
-            <h2>Category</h2>
+            <h2>This Category has news {news.length}</h2>
         </div>
     );
 };
